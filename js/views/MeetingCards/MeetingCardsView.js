d054ed56c36a64ca75fed6b1f3efd5caf95c1a01
alfredkam <kam.alfred@gmail.com>
2013-05-05 12:09:38 -0700

    added twitter handler + quick follow up

--- js/views/MeetingCards/MeetingCardsView.js
+++ js/views/MeetingCards/MeetingCardsView.js
@@ -58,7 +58,32 @@ define([
 							var self = this;
 							var element = e.currentTarget;
 							$(element).parent().children(".card-extended").slideToggle();
-							
+					});
+					
+					$(".popUp-btn").click(function(e){
+						e.stopPropagation();
+						var self = this;
+						var name = $(this).attr("data-name");
+						$("#overlay-message").attr("placeholder",
+						"I wrote a follow up message for your meeting yesterday! Here it is:"+
+									 
+						"Hi there, I really enjoyed meeting up with you yesterday. Thanks again for taking some time to get together and share ideas."+
+									 
+						"Looking forward to keeping in touch in the future,"+
+									 
+						"Andrew"		
+						
+						);
+						
+						$(".overlay-name").html(name);
+						$(".overlay").fadeIn();
+						
+					});
+					$("#overlay-send").click(function(){
+						$(".overlay").fadeOut();
+					});
+					$("#overlay-cancel").click(function(){
+						$(".overlay").fadeOut();
 					});
 					//$("#notification").notify();
 					//$("#notification").notify("create", { title : "hello" , text : "You have a meeting coming up" });
